32f8672bda610227678c9e5221d96ef3d33ba485
coffeekitkat <82024111+coffeekitkat@users.noreply.github.com>
2025-05-06 22:30:51 +0800

    chore: fix cjs

--- src/dotenv.ts
+++ src/dotenv.ts
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import { telemetry } from './telemetry';
 
-// eslint-disable-next-line ts/explicit-function-return-type
-export default function (config?: dotenv.DotenvConfigOptions) {
-  telemetry().then(() => {}).catch(() => {});
-  return dotenv.config(config);
-}
+telemetry().then(() => {}).catch(() => {});
+export { dotenv as default };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,67 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
+export default defineConfig([
+  // {
+  //   entry: ['src/dotenv.ts'],
+  //   format: ['esm'],
+  //   outDir: 'dist',
+  //   dts: true,
+  //   outExtension: () => ({ js: '.js' }),
+  //   clean: false,
+  //   splitting: false,
+  //   target: 'esnext',
+  // },
+  // {
+  //   entry: ['src/dotenv.cjs.ts'],
+  //   format: ['cjs'],
+  //   outDir: 'dist',
+  //   dts: true,
+  //   outExtension: () => ({ js: '.cjs' }),
+  //   clean: false,
+  //   splitting: false,
+  //   target: 'esnext',
+  // },
+  {
   entry: [
     'src/index.ts',
     'src/nuxt.ts',
     'src/cyph.ts',
     'src/si.ts',
-    'src/dotenv.ts',
     'src/utils.ts',
+    'src/dotenv.ts'
   ],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
   shims: true,
   target: 'esnext',
   sourcemap: false,
   minify: true,
   minifySyntax: true,
   minifyIdentifiers: true,
-});
+  cjsInterop: true,
+  splitting: true,
+  // footer(ctx) {
+  //   if (ctx.format === 'cjs') {
+  //     return {
+  //       js: 'module.exports = module.exports.default;',
+  //     };
+  //   }
+  // },
+  // banner(ctx) {
+  //   if (ctx.format === 'esm') {
+  //     return {
+  //       js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+  //     };
+  //   }
+  // },
+  // esbuildOptions: (options) => {
+  //   options.footer = {
+  //     // This will ensure we can continue writing this plugin
+  //     // as a modern ECMA module, while still publishing this as a CommonJS
+  //     // library with a default export, as that's how ESLint expects plugins to look.
+  //     // @see https://github.com/evanw/esbuild/issues/1182#issuecomment-1011414271
+  //     js: 'module.exports = module.exports.default;',
+  //   };
+  // },
+}]);
